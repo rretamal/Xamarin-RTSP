cd79db87b59bb40fb009c6993cd55d6dc4270c88
rretamal <rretamal.dev@gmail.com>
2021-06-17 21:47:15 -0400

    feat(main): Add RtspClient implementation

---
+++ Xamarin.Rtsp/Xamarin.Rtsp.Android/Core/NetUtils.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Java.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Xamarin.Rtsp.Droid.Core
+{
+    public class NetUtils
+    {
+        public static Socket CreateSocketAndConnect(string name, int port, int timeout)
+        {
+            var socket = new Socket();
+            socket.Connect(new InetSocketAddress(name, port));
+            socket.SetSoLinger(false, 1);
+            socket.SoTimeout = timeout;
+
+            return socket;
+        }
+    }
+}
--- Xamarin.Rtsp/Xamarin.Rtsp.Android/Renderers/RtspClient.cs
+++ Xamarin.Rtsp/Xamarin.Rtsp.Android/Renderers/RtspClient.cs
@@ -5,11 +5,13 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Com.Alexvas.Rtsp;
+using Java.Interop;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Xamarin.Forms;
+using Xamarin.Rtsp.Droid.Core;
 using Xamarin.Rtsp.Renderers;
 
 [assembly: Dependency(typeof(RtspClient))]
@@ -21,13 +23,91 @@ namespace Xamarin.Rtsp.Droid.Renderers
 
         public bool StartStreaming()
         {
-            // Java.Net.Socket rtspSocket
-            // string uriRtsp
-            // Java.Util.Concurrent.Atomic.AtomicBoolean rtspStopped
-            // Com.Alexvas.Rtsp.RtspClient.IRtspClientListener
-            // localClient = Com.Alexvas.Rtsp.RtspClient.Builder();
+            var socket = NetUtils.CreateSocketAndConnect("rtsp://192.168.1.51", 554, 5000);
+            string uri = "rtsp://192.168.1.51";
+            int port = 554;
+            Java.Util.Concurrent.Atomic.AtomicBoolean rtspStopped = new Java.Util.Concurrent.Atomic.AtomicBoolean(true);
+
+            localClient = new Com.Alexvas.Rtsp.RtspClient.Builder(socket, uri, rtspStopped, null)
+                .RequestVideo(true).RequestAudio(false).WithDebug(true).WithCredentials("admin", "ksec2021").Build();
+            localClient.Execute();
+
+            //NetUtils.CloseSocket(socket);
 
             return false;
+        }
+    }
+
+    public class MyListener : Java.Lang.Object, Com.Alexvas.Rtsp.RtspClient.IRtspClientListener
+    {
+        public IntPtr Handle => throw new NotImplementedException();
+
+        public int JniIdentityHashCode => throw new NotImplementedException();
+
+        public JniObjectReference PeerReference => throw new NotImplementedException();
+
+        public JniPeerMembers JniPeerMembers => throw new NotImplementedException();
+
+        public JniManagedPeerStates JniManagedPeerState => throw new NotImplementedException();
+
+        public void Dispose()
+        {
+        }
+
+        public void Disposed()
+        {
+        }
+
+        public void DisposeUnlessReferenced()
+        {
+        }
+
+        public void Finalized()
+        {
+        }
+
+        public void OnRtspAudioSampleReceived(byte[] data, int offset, int length, long timestamp)
+        {
+        }
+
+        public void OnRtspConnected(Com.Alexvas.Rtsp.RtspClient.SdpInfo sdpInfo)
+        {
+        }
+
+        public void OnRtspConnecting()
+        {
+        }
+
+        public void OnRtspDisconnected()
+        {
+        }
+
+        public void OnRtspFailed(string message)
+        {
+        }
+
+        public void OnRtspFailedUnauthorized()
+        {
+        }
+
+        public void OnRtspVideoNalUnitReceived(byte[] data, int offset, int length, long timestamp)
+        {
+        }
+
+        public void SetJniIdentityHashCode(int value)
+        {
+        }
+
+        public void SetJniManagedPeerState(JniManagedPeerStates value)
+        {
+        }
+
+        public void SetPeerReference(JniObjectReference reference)
+        {
+        }
+
+        public void UnregisterFromRuntime()
+        {
         }
     }
 }
